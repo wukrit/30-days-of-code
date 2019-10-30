2ba099ee94cc01d63ebee12a8d801fda7148af6b
wukrit <sukritwalia@gmail.com>
2019-10-29 20:43:02 -0400

    01 done

---
+++ 01-data-types.rb
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int = gets.chomp.to_i
+double = gets.chomp.to_f
+str = gets.chomp.to_s
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+puts i + int
+# Print the sum of the double variables on a new line.
+puts d + double
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + str
