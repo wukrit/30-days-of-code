c3a142fd6b20e2f4c8c7eecfddaa3ef60b89012d
Wukrit <sukritwalia@gmail.com>
2019-11-17 21:30:15 -0500

    prettified 12, added main function

--- 12-inheritance.js
+++ 12-inheritance.js
@@ -1,63 +1,93 @@
-'use strict';
+"use strict";
 
-var _input = '';
+var _input = "";
 var _index = 0;
-process.stdin.on('data', (data) => { _input += data; });
-process.stdin.on('end', () => {
-    _input = _input.split(new RegExp('[ \n]+'));
-    main();
+process.stdin.on("data", data => {
+  _input += data;
 });
-function read() { return _input[_index++]; }
+process.stdin.on("end", () => {
+  _input = _input.split(new RegExp("[ \n]+"));
+  main();
+});
+function read() {
+  return _input[_index++];
+}
 
 /**** Ignore above this line. ****/
 
 class Person {
-    constructor(firstName, lastName, identification) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.idNumber = identification;
-    }
+  constructor(firstName, lastName, identification) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = identification;
+  }
 
-    printPerson() {
-        console.log(
-            "Name: " + this.lastName + ", " + this.firstName
-            + "\nID: " + this.idNumber
-        )
-    }
+  printPerson() {
+    console.log(
+      "Name: " +
+        this.lastName +
+        ", " +
+        this.firstName +
+        "\nID: " +
+        this.idNumber
+    );
+  }
 }
 
 class Student extends Person {
-    /*
-    *   Class Constructor
-    *
-    *   @param firstName - A string denoting the Person's first name.
-    *   @param lastName - A string denoting the Person's last name.
-    *   @param id - An integer denoting the Person's ID number.
-    *   @param scores - An array of integers denoting the Person's test scores.
-    */
-    // Write your constructor here
-    constructor(firstName, lastName, id, scores) {
-        super()
-        this.firstName = firstName
-        this.lastName = lastName
-        this.idNumber = id
-        this.scores = scores
+  /*
+   *   Class Constructor
+   *
+   *   @param firstName - A string denoting the Person's first name.
+   *   @param lastName - A string denoting the Person's last name.
+   *   @param id - An integer denoting the Person's ID number.
+   *   @param scores - An array of integers denoting the Person's test scores.
+   */
+  // Write your constructor here
+  constructor(firstName, lastName, id, scores) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = id;
+    this.scores = scores;
+  }
+  /*
+   *   Method Name: calculate
+   *   @return A character denoting the grade.
+   */
+  // Write your method here
+  calculate() {
+    let sum = this.scores.reduce((sum, score) => (sum += score));
+    let avg = sum / this.scores.length;
+    if (avg >= 90) {
+      return "O";
+    } else if (avg >= 80) {
+      return "E";
+    } else if (avg >= 70) {
+      return "A";
+    } else if (avg >= 55) {
+      return "P";
+    } else if (avg >= 40) {
+      return "D";
+    } else {
+      return "T";
     }
-    /*
-    *   Method Name: calculate
-    *   @return A character denoting the grade.
-    */
-    // Write your method here
-    calculate() {
-        let sum = this.scores.reduce( (sum, score) => sum += score )
-        let avg = sum / this.scores.length
-        if (avg >= 90) { return 'O' }
-        else if (avg >= 80) { return 'E' }
-        else if (avg >= 70) { return 'A' }
-        else if (avg >= 55) { return 'P' }
-        else if (avg >= 40) { return 'D' }
-        else { return 'T' }
-    }
+  }
 }
 
 function main() {
+  let firstName = read();
+  let lastName = read();
+  let id = +read();
+  let numScores = +read();
+  let testScores = new Array(numScores);
+
+  for (var i = 0; i < numScores; i++) {
+    testScores[i] = +read();
+  }
+
+  let s = new Student(firstName, lastName, id, testScores);
+  s.printPerson();
+  s.calculate();
+  console.log("Grade: " + s.calculate());
+}
